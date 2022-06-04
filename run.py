4c1a7536173abe5420cf172e4aca5abb9704223e
Uniaut <kunwoo0927@gmail.com>
2022-06-04 17:40:08 +0900

    test

--- run.py
+++ run.py
@@ -1,29 +1,32 @@
 import cv2
 import flat
-import tk_test
+import change
 
 
-def process(image: cv2.Mat):
+def process(original: cv2.Mat, image: cv2.Mat):
     try:
         # flat.process(image)
-        tk_test.process(image)
+        change.process(original, image)
     except Exception as e:
         print(e)
         pass
 
 def main():
-    capture = cv2.VideoCapture(2, cv2.CAP_DSHOW)
+    capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     cv2.destroyAllWindows()
+    _, original = capture.read()
     while True:
-        ret, frame = capture.read()
+        _, frame = capture.read()
         frame = cv2.resize(frame, (0, 0), fx=1.0, fy=1.0)
-        process(frame)
-        if cv2.waitKey(25) == 27:
+        process(original, frame)
+        keycode = cv2.waitKey(25)
+        if keycode == 27:
             break
-
+        elif keycode == ord('d'):
+            original = frame
+        
 
 if __name__ == '__main__':
     print(cv2.__version__)
-    tk_test.process(cv2.imread('../img/test.jpg', cv2.IMREAD_ANYCOLOR))
     cv2.waitKey(0)
     main()
--- tk_test.py
+++ tk_test.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 
 
-def line_intersection(line1, line2, polar=False):
+def _line_intersection(line1, line2, polar=False):
     if not polar:
         line1 = line1.reshape(2, 2)
         line2 = line2.reshape(2, 2)
@@ -34,71 +34,47 @@ def line_intersection(line1, line2, polar=False):
         return [x0, y0]
 
 
-def process(image: cv2.Mat):
-    original = image.copy()
-    lsd = cv2.createLineSegmentDetector(0)
-    lines = lsd.detect(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))[0]
-    drawn_image = lsd.drawSegments(image, lines)
-    cv2.imshow('1. original', drawn_image)
+def get_ui_image(image: cv2.Mat):
+    ui_image = image.copy()
+    ui_image = cv2.circle(ui_image, ((image.shape[1] // 2), (image.shape[0] // 2)), 15, (255, 0, 255))
+    return ui_image
 
-    blurred = cv2.GaussianBlur(image, (3, 3), 0)
+
+def get_edge_image(image: cv2.Mat):
+    image = image.copy()
+    blurred = cv2.GaussianBlur(image, (5, 5), 0)
     blurred_split = cv2.split(blurred)
     edges_split = tuple(map(lambda img: cv2.Canny(img, 50, 200), blurred_split))
-    edges = cv2.merge(edges_split)
-    cv2.imshow('2. edges', edges)
-
+    cv2.imshow('Merged RGB', cv2.merge(edges_split))
     edges = sum(edges_split)
-    segmented_edges = edges
+    return edges
 
-    if True:
-        mask = np.zeros((image.shape[0] + 2, image.shape[1] + 2), np.uint8)
-        edges = cv2.dilate(edges, np.ones((3, 3), np.uint8), iterations=2)
-        arbitrary = 150  # any value except 255 because edges are 255
-        for dx, dy in itertools.product([-10, 0, 10], [-10, 0, 10]):
-            starting_point = ((edges.shape[1] // 2) + dx, (edges.shape[0] // 2) + dy)
-            flooded = cv2.floodFill(edges, mask, starting_point, arbitrary)[1]
-        
-        flooded_ = flooded.copy()
-        for dx, dy in itertools.product([-10, 0, 10], [-10, 0, 10]):
-            starting_point = ((edges.shape[1] // 2) + dx, (edges.shape[0] // 2) + dy)
-            flooded_ = cv2.circle(flooded_, starting_point, 3, (0, 0, 255))
-        cv2.imshow('flooded', flooded_)
 
-        flooded_mask = np.zeros((flooded.shape))
-        flooded_mask[edges == arbitrary] = 255
-        # cv2.imshow('flooded_mask', flooded_mask)
+def get_flood_edges(image: cv2.Mat):
+    edges = image.copy()
+    mask = np.zeros((image.shape[0] + 2, image.shape[1] + 2), np.uint8)
+    # edges = cv2.dilate(image, np.ones((3, 3), np.uint8), iterations=2)
+    arbitrary = 150  # any value except 255 because edges are 255
 
-        segmented_edges = cv2.Canny(np.uint8(flooded_mask), 100, 200, L2gradient=True)
+    for dx, dy in itertools.product([-10, 0, 10], [-10, 0, 10]):
+        starting_point = ((edges.shape[1] // 2) + dx, (edges.shape[0] // 2) + dy)
+        flooded = cv2.floodFill(edges, mask, starting_point, arbitrary)[1]
     
-    
-    lines = lsd.detect(segmented_edges.astype('uint8'))[0]
-    drawn_image = lsd.drawSegments(segmented_edges, lines)
-    cv2.imshow('drawn_image', drawn_image)
+    flooded_ = flooded.copy()
+    for dx, dy in itertools.product([-10, 0, 10], [-10, 0, 10]):
+        starting_point = ((edges.shape[1] // 2) + dx, (edges.shape[0] // 2) + dy)
+        flooded_ = cv2.circle(flooded_, starting_point, 3, (0, 0, 255))
+    cv2.imshow('flooded', flooded_)
 
-    # 밑에 코드에서 오류 발생해서 주석처리
+    flooded_mask = np.zeros((flooded.shape))
+    flooded_mask[edges == arbitrary] = 255
 
+    segmented_edges = cv2.Canny(np.uint8(flooded_mask), 100, 200, L2gradient=True)
+    return segmented_edges
+
+
+def show_hough_lines(image, lines, name):
     plane = np.zeros(image.shape)
-    lines = cv2.HoughLines(
-        segmented_edges, 1, np.pi / 180, 100, None, 0, 0
-    )  # hyper-parameter tuned using grid search then manually!
-
-    lines_dict = dict()
-    for i in range(len(lines)):
-        rho = lines[i][0][0]
-        rho = round(rho * 1 / (image.shape[0]), 0)
-        theta = lines[i][0][1]
-        theta = round(theta * 4 / (2 * np.pi), 0)
-        # print('Value: ', rho, theta)
-        lines_dict[rho, theta] = lines[i]
-    else:
-        lines = list(lines_dict.values())
-
-    if len(lines) != 4:
-        return
-        print('number of lines', num_lines)
-        for a, b in lines_dict.items():
-            print(a)
-    
     for i in range(len(lines)):
         rho = lines[i][0][0]
         theta = lines[i][0][1]
@@ -109,12 +85,59 @@ def process(image: cv2.Mat):
         pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
         pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
         cv2.line(plane, pt1, pt2, (0, 255, 0), 1, cv2.LINE_AA)
-    cv2.imshow('plane1.5', plane)
+    cv2.imshow(name, plane)
+
+
+def process(image: cv2.Mat):
+    # UI image
+    ui_image = get_ui_image(image)
+    cv2.imshow('1. original', ui_image)
+
+    # Canny by rgb
+    edges = get_edge_image(image)
+    cv2.imshow('2. edges', edges)
+
+    # segment center objects by flooding
+    segmented_edges = get_flood_edges(edges)
+    cv2.imshow('3. flood center & segment', segmented_edges)
+
+    plane = np.zeros(image.shape)
+    lines = cv2.HoughLines(
+        segmented_edges, 1, np.pi / 180, 100, None, 0, 0
+    )  # hyper-parameter tuned using grid search then manually!
+    show_hough_lines(image, lines, 'original')
+
+    '''
+    for all lines sorted by -rho
+    '''
+    lines_sorted = sorted(list(lines), key=lambda l: l[0][0])
+    # print(lines_sorted)
+
+    lines_dict = dict()
+    for i in range(len(lines_sorted)):
+        rho = lines_sorted[i][0][0]
+        rho = round(rho * 1 / (image.shape[0]), 0)
+        theta = lines_sorted[i][0][1]
+        theta = round(theta * 4 / (2 * np.pi), 0)
+        # print('Value: ', rho, theta)
+        if lines_dict.get((rho, theta)) is None:
+            lines_dict[rho, theta] = lines_sorted[i]
+    else:
+        lines = list(lines_dict.values())
+
+    show_hough_lines(image, lines, 'compressed')
+
+    if len(lines) != 4:
+        print('number of lines', len(lines))
+        for a, b in lines_dict.items():
+            print(a)
+        return
+    
 
     # line_intersection
     points_ = []
     for l1, l2 in itertools.combinations(lines, r=2):
-        xy = tuple(line_intersection(l1, l2, polar=True))
+        xy = tuple(_line_intersection(l1, l2, polar=True))
         if xy[0] < 0 or xy[1] < 0 or xy[0] > plane.shape[1] or xy[1] > plane.shape[0]:
             continue
         points_.append(xy)
@@ -141,6 +164,6 @@ def process(image: cv2.Mat):
     dst_points = np.float32([[0, 0], [dst_size, 0], [0, 1.414 * dst_size], [dst_size, 1.414 * dst_size]])
 
     matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-    result = cv2.warpPerspective(original, matrix, (dst_size, int(1.414 * dst_size)))
+    result = cv2.warpPerspective(image, matrix, (dst_size, int(1.414 * dst_size)))
 
     cv2.imshow('im1reg', result)
